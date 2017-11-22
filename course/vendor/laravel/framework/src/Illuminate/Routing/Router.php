e70b48889deb45e5bdda07deffa01a90fb16ad63
Hady Maher <hadymaher31@yahoo.com>
2017-11-22 13:03:46 +0200

    student controller 2

--- course/vendor/laravel/framework/src/Illuminate/Routing/Router.php
+++ course/vendor/laravel/framework/src/Illuminate/Routing/Router.php
@@ -1107,19 +1107,19 @@ class Router implements RegistrarContract, BindingRegistrar
     public function auth()
     {
         // Authentication Routes...
-        $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
-        $this->post('login', 'Auth\LoginController@login');
-        $this->post('logout', 'Auth\LoginController@logout')->name('logout');
+        $this->get('login', 'Student\LoginController@showLoginForm')->name('login');
+        $this->post('login', 'Student\LoginController@login');
+        $this->post('logout', 'Student\LoginController@logout')->name('logout');
 
         // Registration Routes...
-        $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-        $this->post('register', 'Auth\RegisterController@register');
+        $this->get('register', 'Student\RegisterController@showRegistrationForm')->name('register');
+        $this->post('register', 'Student\RegisterController@register');
 
         // Password Reset Routes...
-        $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-        $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-        $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-        $this->post('password/reset', 'Auth\ResetPasswordController@reset');
+        $this->get('password/reset', 'Student\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+        $this->post('password/email', 'Student\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+        $this->get('password/reset/{token}', 'Student\ResetPasswordController@showResetForm')->name('password.reset');
+        $this->post('password/reset', 'Student\ResetPasswordController@reset');
     }
 
     /**
